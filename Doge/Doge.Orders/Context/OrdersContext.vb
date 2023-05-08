62eee98277c93628c1ebbe70df2aeae174440ab2
Guillermo Rivera <foowillx@gmail.com>
2023-05-08 12:56:57 -0600

    GRC-CC002 Windows Service
    Windows service added
    
    -Retrieve orders function added
    -DB context added
    -Connection added

---
+++ Doge/Doge.Orders/Context/OrdersContext.vb
@@ -0,0 +1,12 @@
+﻿Imports Doge.Model
+Imports System.Data.Entity
+
+Public Class OrdersContext
+    Inherits DbContext
+
+    Public Property Orders As DbSet(Of Order)
+
+    Public Sub New(connectionString As String)
+        MyBase.New(connectionString)
+    End Sub
+End Class
--- Doge/Doge.Orders/Service1.vb
+++ Doge/Doge.Orders/Service1.vb
@@ -1,4 +1,7 @@
-﻿Public Class Service1
+﻿Imports Doge.Model
+Imports System.Configuration
+
+Public Class Service1
 
     Protected Overrides Sub OnStart(ByVal args() As String)
         ' Add code here to start your service. This method should set things
@@ -8,5 +11,18 @@
     Protected Overrides Sub OnStop()
         ' Add code here to perform any tear-down necessary to stop your service.
     End Sub
+
+    Public Function GetOrders() As List(Of Order)
+        Dim orders As New List(Of Order)()
+        ' Get connection string to DB from Appconfig.
+        Dim connectionString As String = ConfigurationManager.ConnectionStrings("DOGEEntities").ConnectionString
+        ' Conectar a la base de datos.
+        Using db As New OrdersContext(connectionString)
+            ' Obtener todas las órdenes de la tabla "Orders".
+            orders = db.Orders.ToList()
+        End Using
+
+        Return orders
+    End Function
 
 End Class
